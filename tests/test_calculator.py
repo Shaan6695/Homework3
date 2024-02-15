6b1725afb6f3f784e090c2f4678c45e304151db2
Shaan6695 <sam33@njit.edu>
2024-02-14 22:35:23 -0500

    added multiply

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,15 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, multiply
 
 def test_addition():
     '''Test that addition function works '''    
     assert add(2,2) == 4
 
 def test_subtraction():
-    '''Test that subtraction function works '''    
+    '''Testing1 that subtrdaction function works '''    
     assert subtract(2,2) == 0
+
+def test_multiply():
+    """Testing multiply"""
+    assert multiply(2,2) == 4
     
