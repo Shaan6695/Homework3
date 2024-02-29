ba5182a4e620dc48be85811b82fcfe36e4933905
Shaan6695 <sam33@njit.edu>
2024-02-29 15:20:39 -0500

    main.py

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -12,7 +12,7 @@ class CommandHandler:
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
 
-   """ def execute_command(self, command_name: str):
+    """ def execute_command(self, command_name: str):
         if command_name in self.commands:
             self.commands[command_name].execute()
         else:
