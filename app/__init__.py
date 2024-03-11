678b08ee1fd3cdca5e4737edcd6c35e865748455
Shaan6695 <sam33@njit.edu>
2024-03-10 22:48:31 -0400

    updating plugins

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,52 @@
+import pkgutil
+import importlib
 from app.commands import CommandHandler
-from app.commands.exit import ExitCommand
-from app.commands.goodbye import GoodbyeCommand
-from app.commands.greet import GreetCommand
+from app.commands import Command
+from app.plugins.menu import MenuCommand
+import multiprocessing
+from dotenv import load_dotenv
+import os
+
+
 
 class App:
-    def __init__(self): # Constructor
+    def __init__(self):
+        load_dotenv()
+        self.settings = {}  # Initialize settings as an empty dictionary
+        # Load all environment variables into settings
+        for key, value in os.environ.items():
+            self.settings[key] = value
+        # Default to 'PRODUCTION' if 'ENVIRONMENT' not set
+        self.settings.setdefault('ENVIRONMENT', 'TESTING')   
         self.command_handler = CommandHandler()
-        # Register commands here
+    def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
+        return self.settings[env_var]
 
+    def load_plugins(self):
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue
+
+        self.command_handler.register_command("menu", MenuCommand(self.command_handler))
 
     def start(self):
-        self.command_handler.register_command("greet", GreetCommand())
-        self.command_handler.register_command("goodbye", GoodbyeCommand())
-        self.command_handler.register_command("exit", ExitCommand())
-
-        
+        self.load_plugins()
         print("Type 'exit' to exit.")
-        while True:  #REPL Read, Evaluate, Process, Loop
+        while True:
             self.command_handler.execute_command(input(">>> ").strip())
 
+def start_app():
+    app = App()
+    app.start()
 
+if __name__ == "__main__":
+    multiprocessing.Process(target=start_app).start()
 
